75fc74422ace60bd4a406b383f1dfda281d0fdd3
gustav0lopez <lopezmendozagustavo@gmail.com>
2022-06-30 16:46:42 -0300

    Condicionales

---
+++ sentencias_de_control.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args){
+        int puertas = 1 ;
+        if (puertas>0){
+            System.out.println("Es mayor que 0");
+        }
+
+    }
+
+}
